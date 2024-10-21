adae3d29883615c711e969306fa15fedc328dd45
山雨欲来风满楼 <maqingshui@outlook.com>
2024-10-21 08:54:14 +0800

    修改输入的合法性判断，使其更严格
    原有代码输入ABC、123ABC时均不会触发合法性驳回，存在较大问题，此次修改通过对scanf添加缓冲区监测实现了对上述情况的约束。

--- P.68_3.1_1.c
+++ P.68_3.1_1.c
@@ -5,27 +5,28 @@
 void main()
 {
     int num, hundreds, tens, ones, reversed;
+    char check;
     
     SetConsoleOutputCP(65001);  //定义Shell编码为UTF-8以防乱码
 
     start: //定义goto标签
     printf("请输入一个三位整数：");
-    scanf("%d", &num);
-    
+    scanf("%d%c", &num, &check);
+
     hundreds = num / 100;
     tens = (num / 10) % 10;
     ones = num % 10;
-
-    if (hundreds==0 || hundreds >=10){
+    
+    //输入合法性判断
+    if (hundreds==0 || hundreds >=10 || check!='\n'){
         printf("输入错误，请输入一个三位整数！\n");
+        while(getchar() != '\n');
         goto start;
     }
     
     reversed = ones * 100 + tens * 10 + hundreds;
-    printf("逆序数是：%d\n", reversed);
+    printf("逆序数是：%d", reversed);
 
-    // 清除输入缓冲区，防止 scanf 后残留的换行符影响 getchar
-    while (getchar() != '\n');
-    printf("按任意键退出...");
+    printf("按任意键退出...")
     getchar();
 }
